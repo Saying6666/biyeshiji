80ce8c27ae7cf611478ecde44549e621c13025dc
Saying6666 <913823230@qq.com>
2024-02-24 14:56:03 +0800

    提交

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login_view'),
     path('index/', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,5 @@
+from django.conf import UserSettingsHolder
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 
 # Create your views here.
@@ -5,22 +7,61 @@ from django.shortcuts import redirect, render
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
+@login_required
 def home(request):
     return render(request, 'home.html')
 
-def login(request):#登录
-    print(request.method)
-    if request.method == 'POST':#如果是post请求
-        #form = AuthenticationForm(data=request.POST)#获取表单数据
-        print(request.POST)
-        #if form.is_valid():#如果表单数据有效
-         #   return redirect('home')#重定向到主页
-    
-    return render(request, 'login.html', {'form': AuthenticationForm()})#返回登录页面
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
+
+def login_view(request):#登录
+    print(request.POST)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user=authenticate(request,username=username, password=password)
+        if user is not None:
+            print('登录成功')
+            login(request, user)
+            print('登录成功2')
+            return JsonResponse({'code': 0, 'msg': '登录成功'})#登录成功返回主页
+        else:
+            return render(request, 'login.html', {'error': '用户名或密码错误'})
+    else:
+     return render(request, 'login.html')
 
 def index(request):
     return render(request, 'index.html');
 
+
 def register(request):
-    return render(request, 'register.html');
+    print(request.POST)
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    if not email or not password:
+        return render(request, 'register.html', {'error': '邮箱或密码不能为空'})
+
+    # 判断邮箱是否已经注册
+    if User.objects.filter(email=email).exists():
+        print('邮箱已经注册')
+        return JsonResponse({'code': 1, 'msg': '邮箱已经注册'})
+
+    user = User.objects.create_user(username=email, email=email, password=password)
+    user.save()
+
+    user = authenticate(username=email, password=password)
+    if user is not None:
+        login_view(request)
+
+    
+
+    return JsonResponse({'code': 0, 'msg': '注册成功'})#注册成功返回主页
